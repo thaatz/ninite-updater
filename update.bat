9d83524fc4667c239577fd420afd3aad539636a0
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-11-23 10:32:28 -0500

    finalized the commands
    removed the test echos

--- update.bat
+++ update.bat
@@ -8,20 +8,20 @@ set folder=%userprofile%\desktop\Programs
 :: ========================================
 
 ::we run ninite and then wait for it to close
-REM "%ninite%"
+"%ninite%"
 REM for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "%~dp0*.lnk"') do MOVESOMWHERE "%%F"
 :: This would be the windows xp workaround
 :: http://stackoverflow.com/questions/17616045/find-files-on-windows-modified-after-a-given-date-using-the-command-line
 :: forfiles requires windows vista+
 :: https://technet.microsoft.com/en-us/library/cc753551(v=ws.11).aspx
-forfiles /p "%public%\Desktop" /M *.exe /d +%date:~4% >nul 2>nul
+forfiles /p "%public%\Desktop" /M *.lnk /d +%date:~4% >nul 2>nul
 if %errorlevel%==1 (
 	echo ninite didnt install nayfin neww
 	pause
 	exit
 )
-for /f "eol=: delims=" %%F in ('forfiles /p "%public%\Desktop" /M *.exe /d +%date:~4%') do echo %public%\Desktop\%%F "%folder%"
-REM pause 
+for /f "eol=: delims=" %%F in ('forfiles /p "%public%\Desktop" /M *.lnk /d +%date:~4%') do move %public%\Desktop\%%F "%folder%"
+pause 
 
 
 :: We need to go to the public desktop and then run the dir command otherwise it will list files in your working directory as well
